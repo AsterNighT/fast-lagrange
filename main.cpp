48ee0d1386226bb7326c8dc5a7d5a45fcacfb333
Th0masAndy <yangxinpeng@zju.edu.cn>
2023-11-29 16:28:44 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include "nttinterpolator.h"
 std::vector<int> x;
 std::vector<std::vector<int>> y;
-int main()
-{
-    NTT::NTTInterpolator *ntt = new NTT::NTTInterpolator();
-    int n = 1300;
-    x = std::vector<int>(n);
-    for (int i = 0; i < 4; i++)
-        y.push_back(std::vector<int>(n));
+int main() {
+  NTT::NTTInterpolator::init();
+  NTT::NTTInterpolator *ntt = new NTT::NTTInterpolator();
 
-    for (int i = 1; i <= n; i++)
-    {
-        x[i] = rand() % NTT::mod;
-        for (int j = 0; j < 4; j++)
-            y[j][i] = rand() % NTT::mod;
-    }
-    long start = clock();
+  int n = 1000;
+  x = std::vector<int>(n + 1);
+  for (int i = 0; i < 4; i++)
+    y.push_back(std::vector<int>(n + 1));
+
+  for (int i = 1; i <= n; i++) {
+    x[i] = rand() % NTT::mod;
+    for (int j = 0; j < 4; j++)
+      y[j][i] = rand() % NTT::mod;
+  }
+  long start = clock();
+
+  for (auto k = 0; k < 8192 * 5; k++) {
     ntt->init_with_params(n, x);
     std::vector<std::vector<int>> ans = ntt->fast_lagrange(y);
-    printf("%ld\n", (clock() - start) * 1000 / CLOCKS_PER_SEC);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        if (y[i][1] != NTT::F(ans[i], x[1]))
-            printf("Bad\n");
-    }
-    return 0;
+  }
+
+  printf("%ld ms\n", (clock() - start) * 1000 / CLOCKS_PER_SEC);
+  //   for (int i = 0; i < ans.size(); i++) {
+  //     for (int j = 1; j <= n; j++) {
+  //       if (y[i][j] != NTT::F(ans[i], x[j]))
+  //         printf("Bad %d %d\n", i, j);
+  //     }
+  //   }
+  return 0;
 }
